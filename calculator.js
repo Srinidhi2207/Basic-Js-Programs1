63986cc955ab42ed184b29f73ee372d1cd269f97
chinni <23VE1A0552@sreyas.ac.in>
2026-05-04 14:36:14 +0530

    evenodd

---
+++ calculator.js
@@ -0,0 +1,18 @@
+let num1=12
+let num2=90
+let operator='+';
+switch(operator)
+{
+    case '+':
+    console.log("addition of 2 numbers:"+(num1+num2));
+    break;
+    case '-':
+    console.log("subtraction of 2 numbers:"+(num1-num2));
+    break;
+    case '*':
+    console.log("multiplication of 2 numbers:"+(num1*num2));
+    break;
+    case '/':
+    console.log("division of 2 numbers:"+(num1/num2));
+    break;
+}
